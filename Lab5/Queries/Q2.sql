8ebd71c4373b0340a16f367ba4abda79287566d9
BB1101 <91317427+BB1101@users.noreply.github.com>
2022-03-21 00:48:35 +0800

    Update Q2.sql

--- Lab5/Queries/Q2.sql
+++ Lab5/Queries/Q2.sql
@@ -1,5 +1,5 @@
 select product_name
-from products-in-orders 
+from PRODUCTS-IN-ORDERS
 where SUM(quantity_on_order)>=100 AND feedback_rating_score=5 AND delivery_date >= '2021-8-1' AND delivery_date <= '2021-8-31' 
 group by product_name
 order by AVG(feedback_rating_score) ASC
