342dae1898d5134ac4f07ba21dd601aebf487f99
BB1101 <91317427+BB1101@users.noreply.github.com>
2022-03-21 10:24:43 +0800

    Update Q3.sql

--- Lab5/Queries/Q3.sql
+++ Lab5/Queries/Q3.sql
@@ -1,3 +1,3 @@
 select avg(delivery_date - order_timestsmp) 
-from PRODUCTS-IN-ORDERS, ORDERS
-where order_timestsmp >= '2021-6-1' AND order_timestsmp <= '2021-6-30' AND delivery_date <= now() AND PRODUCTS-IN-ORDERS.order_id=ORDERS.order_id
+from PRODUCTS-IN-ORDER left join ORDERS on PRODUCTS-IN-ORDERS.order_id=ORDERS.order_id
+where order_timestsmp >= '2021-6-1' AND order_timestsmp <= '2021-6-30' AND delivery_date <= now() 
