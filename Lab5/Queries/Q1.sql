7b33e0c38dc2e6b834861c424cd8e399c9df65a7
ruyi an <anruyi009@gmail.com>
2022-03-23 14:44:49 +0800

    Update query 1) Datetime should use the full format, 2) mismatch in table name

--- Lab5/Queries/Q1.sql
+++ Lab5/Queries/Q1.sql
@@ -1,3 +1,5 @@
- select AVG(price) 
- from PRICE-HISTORYp
- where shop_name='Shiokee' AND product_name='iPhone Xs' AND start_date>='2021-8-1' AND end_date <='2021-8-31'
+SELECT AVG(price) AS AvgPrice
+FROM price_history
+WHERE product_name = 'iPhone X'
+  AND ((start_date >= '2021.08.01 00:00:00' AND start_date < '2021.08.31 00:00:00')
+    OR (end_date >= '2021.08.01 00:00:00' AND end_date < '2021.08.31 00:00:00'));
