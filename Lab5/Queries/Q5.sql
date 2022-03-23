c945df7633f0adf3d3649c87c773ad83a6020a14
ruyi an <anruyi009@gmail.com>
2022-03-23 15:02:01 +0800

    Update Q5 - add in comments

--- Lab5/Queries/Q5.sql
+++ Lab5/Queries/Q5.sql
@@ -1,13 +1,18 @@
--- part 1 select where maker = 'Samsung'
+-- Produce a list that contains
+-- (i) all products made by Samsung, and
+-- (ii) for each of them, the number of shops on Shiokee that sell the product.
+
+-- Part(i) --
 SELECT product_name
 FROM product
 WHERE maker = 'Samsung';
 
+-- Part(ii) --
 SELECT product_name, COUNT(DISTINCT shop_name) AS shopcount
 FROM product_in_shop
 WHERE product_name IN (
-   SELECT product_name
-   FROM product
-   WHERE maker = 'Samsung'   
+    SELECT product_name
+    FROM product
+    WHERE maker = 'Samsung'
 )
 GROUP BY product_name
