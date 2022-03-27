45cee3a360ec4c92146bc3ffea4f866f2d2bfd3f
Zhang Tianyu <zhangtianyu@outlook.sg>
2022-03-27 15:37:09 +0800

    Update Q8_zty.sql

--- Lab5/Queries/Q8_zty.sql
+++ Lab5/Queries/Q8_zty.sql
@@ -2,10 +2,11 @@
 -- products by other users in August 2021.
 
 SELECT TOP 5 WITH TIES t1.product_name, SUM(t1.order_quantity) AS purchased_amount
-FROM users, 
-   product_on_order as t1 JOIN orders as t2 ON t1.order_id = t2.order_id
-WHERE YEAR(t2.order_placing_timestamp) = 2021 
+FROM product_on_order as t1,
+   orders as t2 
+WHERE t1.order_id = t2.order_id
+   AND YEAR(t2.order_placing_timestamp) = 2021 
    AND MONTH(t2.order_placing_timestamp) = 8
 GROUP BY t1.product_name
-HAVING count(distinct t2.user_id) < count(users.user_id)
+HAVING count(distinct t2.user_id) < (SELECT count(distinct user_id) FROM users)
 ORDER BY purchased_amount DESC;
