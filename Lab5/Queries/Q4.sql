6f12372bea16950c2ac42933750274b368a17101
Zhang Tianyu <zhangtianyu@outlook.sg>
2022-03-27 22:07:27 +0800

    Commented

--- Lab5/Queries/Q4.sql
+++ Lab5/Queries/Q4.sql
@@ -1,6 +1,10 @@
 -- Let us define the “latency” of an employee by the average that he/she takes to process a complaint.
 -- Find the employee with the smallest latency.
 
+-- Subquery: latency_record
+-- We first aggregate the latency defined in the question
+-- Then we select the employee(s) by clauses TOP 1 WITH TIES and ORDER BY
+-- Note: We use WITH TIES to allow multiple results
 WITH latency_record AS(
    SELECT TOP 1 WITH TIES employee_id, AVG(CAST(DATEDIFF(second, resolved_timestamp, assigned_timestamp) as FLOAT)) AS latency
    FROM complaint
@@ -8,20 +12,7 @@ WITH latency_record AS(
    ORDER BY latency
 )
 
+-- Fetch all information of the employee(s) from table employee
 SELECT employee_id, employee_name, salary
 FROM employee
 WHERE employee_id IN (SELECT employee_id FROM latency_record)
-
-/*
-DROP TABLE IF EXISTS LatencyRecord
-
-SELECT employee_id, AVG(DATEDIFF(second, file_timestamp, resolved_timestamp)) AS AvgLatency INTO LatencyRecord
-FROM complaint
-GROUP BY employee_id;
-
--- Find all the information of the employee with the smallest latency
-SELECT E.employee_id, E.employee_name, E.salary
-FROM LatencyRecord as L, employee as E
-WHERE L.employee_id = E.employee_id
-   AND AvgLatency = (SELECT MIN(AvgLatency) FROM LatencyRecord);
-*/
--- Lab5/Queries/Q6.sql
+++ Lab5/Queries/Q6.sql
@@ -1,4 +1,11 @@
 -- Find shops that made the most revenue in August 2021.
+
+-- We first join order with product on order to achieve dealing_price and qorder_quantity
+-- and use WHERE clause to filter records in Aug 2021
+-- We then GROUP BY each shop, and aggregate the revenue as: SUM(t2.dealing_price * t2.order_quantity)
+-- Finally, we select the shop(s) made most revenue by clauses TOP 1 WITH TIES and ORDER BY
+-- Note: We use WITH TIES to allow multiple results
+
 SELECT TOP 1 WITH TIES t2.shop_name, SUM(t2.dealing_price * t2.order_quantity) AS revenue
 FROM orders as t1 JOIN product_on_order as t2
 ON t1.order_id = t2.order_id
@@ -8,7 +15,9 @@ GROUP BY t2.shop_name
 ORDER BY revenue DESC
 
 /*
--- Middle step version
+-- Version with subquery
+-- less compact than the single-query version
+
 WITH shop_revenue AS (
   SELECT t2.shop_name, SUM(t2.dealing_price * t2.order_quantity) AS revenue
   FROM orders as t1 JOIN product_on_order as t2
