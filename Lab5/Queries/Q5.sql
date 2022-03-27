8ceae8c7f9c13e730840e453616438f620d7ed4b
current12 <73266307+current12@users.noreply.github.com>
2022-03-27 19:23:13 +0800

    Update Q5.sql

--- Lab5/Queries/Q5.sql
+++ Lab5/Queries/Q5.sql
@@ -11,6 +11,18 @@ WHERE product_name IN (
 )
 GROUP BY product_name
 
+
+/*
+--if there is product made by Samsung and no shop sell it.
+
+SELECT p.product_name, SUM(CASE WHEN pis.product_name IS NULL THEN 0 ELSE 1 END) AS shop_count
+FROM product AS p
+LEFT JOIN product_in_shop AS pis 
+ON p.product_name = pis.product_name
+WHERE p.maker = 'Samsung'
+GROUP BY p.product_name
+*/
+
 /*
 -- Proof Check 
 SELECT product_name
