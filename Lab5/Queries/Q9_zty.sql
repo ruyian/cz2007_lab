3a612ab20ea69fe0b8617c28cd70878b8f4e29a4
Zhang Tianyu <zhangtianyu@outlook.sg>
2022-03-27 13:52:07 +0800

    Update Q9_zty.sql

--- Lab5/Queries/Q9_zty.sql
+++ Lab5/Queries/Q9_zty.sql
@@ -1,18 +1,24 @@
+-- Clarification: we only consider the year 2021
+
 WITH Monthly_count AS(
 SELECT t1.product_name, 
-   --YEAR(t2.order_placing_timestamp) AS Purchased_year, 
+   YEAR(t2.order_placing_timestamp) AS Purchased_year, 
    MONTH(t2.order_placing_timestamp) AS Purchased_Month, 
    SUM(order_quantity) AS Purchased_amount
 FROM product_on_order as t1 JOIN orders as t2 ON t1.order_id = t2.order_id
 GROUP BY t1.product_name, 
-   --YEAR(t2.order_placing_timestamp), 
+   YEAR(t2.order_placing_timestamp), 
    MONTH(t2.order_placing_timestamp)
 )
 
 SELECT t1.product_name
 FROM Monthly_count AS t1, Monthly_count AS t2
 WHERE t1.product_name = t2.product_name
-   AND t1.Purchased_Month = t2.Purchased_Month + 1
+   AND (
+      (t1.Purchased_year = t2.Purchased_year AND t1.Purchased_Month = t2.Purchased_Month + 1)
+      OR
+      (t1.Purchased_year = t2.Purchased_year + 1 AND t1.Purchased_Month = 1 AND t2.Purchased_Month = 12)
+   )
    AND t1.Purchased_amount > t2.Purchased_amount
 GROUP BY t1.product_name
 HAVING count(t1.Purchased_Month) >= 3
