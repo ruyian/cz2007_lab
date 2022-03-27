87d7042d032ae8d1c336e62e74dd6cedea7db593
BB1101 <91317427+BB1101@users.noreply.github.com>
2022-03-27 21:48:41 +0800

    Update Q1.sql

--- Lab5/Queries/Q1.sql
+++ Lab5/Queries/Q1.sql
@@ -2,7 +2,7 @@
 
 SELECT product_name, ROUND(AVG(Cast(actual_price as Float)), 2) AS AvgPrice
 FROM price_history
-WHERE product_name = 'iPhone XS'
-  AND ((start_date >= '2021.08.01 00:00:00' AND start_date < '2021.09.01 00:00:00')
-    OR (end_date >= '2021.08.01 00:00:00' AND end_date < '2021.09.01 00:00:00'))
-GROUP BY product_name; -- Additional Group By clause added to print the product name 'iPhone X'
+WHERE product_name = 'iPhone XS' --select “iPhone Xs”
+  AND ((start_date >= '2021.08.01 00:00:00' AND start_date < '2021.09.01 00:00:00')--select start_date from 1 August 2021 to 31 August 2021
+    OR (end_date >= '2021.08.01 00:00:00' AND end_date < '2021.09.01 00:00:00')) --select end_date from 1 August 2021 to 31 August 2021
+GROUP BY product_name; -- Additional Group By clause added to print the product name 'iPhone XS'
